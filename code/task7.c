b9189dcde8d7495ea9507a0012f8fe0f405a3da4
caelia59 <caelia1166@gmail.com>
2026-08-30 03:04:25 +0800

    task7
    add task7 code

---
+++ code/task7.c
@@ -0,0 +1,134 @@
+#include "stm32f1xx_hal.h"
+#include <stdint.h>
+
+#define BLACK_LEVEL GPIO_PIN_SET
+
+static void GPIO_Init(void)
+{
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_AFIO_CLK_ENABLE();
+
+    __HAL_AFIO_REMAP_SWJ_NOJTAG();
+
+    GPIO_InitTypeDef gpio = {0};
+
+    gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9;
+    gpio.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &gpio);
+
+    gpio.Pin = GPIO_PIN_12 | GPIO_PIN_13;
+    gpio.Mode = GPIO_MODE_INPUT;
+    gpio.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &gpio);
+
+    gpio.Pin = GPIO_PIN_15;
+    gpio.Mode = GPIO_MODE_INPUT;
+    gpio.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(GPIOA, &gpio);
+}
+
+static void Motor_Stop(void)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+}
+
+static void Motor_Forward(void)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+}
+
+static void Turn_Left(void)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+}
+
+static void Turn_Right(void)
+{
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+}
+
+static uint8_t Button_Click(void)
+{
+    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) != GPIO_PIN_RESET)
+    {
+        return 0;
+    }
+
+    HAL_Delay(20);
+
+    if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) != GPIO_PIN_RESET)
+    {
+        return 0;
+    }
+
+    while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_15) == GPIO_PIN_RESET)
+    {
+        HAL_Delay(5);
+    }
+
+    return 1;
+}
+
+int main(void)
+{
+    HAL_Init();
+    GPIO_Init();
+
+    uint8_t running = 0;
+
+    while (1)
+    {
+        if (Button_Click())
+        {
+            running = !running;
+        }
+
+        if (!running)
+        {
+            Motor_Stop();
+            continue;
+        }
+
+        uint8_t left =
+            HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12) == BLACK_LEVEL;
+
+        uint8_t right =
+            HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13) == BLACK_LEVEL;
+
+        if (!left && !right)
+        {
+            Motor_Forward();
+        }
+        else if (left && !right)
+        {
+            Turn_Left();
+        }
+        else if (!left && right)
+        {
+            Turn_Right();
+        }
+        else
+        {
+            Motor_Stop();
+        }
+
+        HAL_Delay(5);
+    }
+}
